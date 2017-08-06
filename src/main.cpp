dd189e7214ea026be4b4a692db80ac2fef7b5309
Major Péter <majorpeter29@gmail.com>
2017-08-06 13:03:57 +0200

    created Color class, implemented bit mask functions for Color->LedStrip

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Hardware.h"
 #include "LedStripController.h"
+#include "Color.h"
 #include "diag/Trace.h"
 
 int main() {
@@ -12,8 +13,9 @@ int main() {
 	LedStripController ledStrip(Hardware::LedStripDataOutPort, Hardware::LedStripDataOutPin);
 	ledStrip.init();
 
+	Color red(255, 0, 0);
 	while(1) {
-		ledStrip.writeLeds();
+		ledStrip.writeLeds(&red, 1);
 	}
 
 	return 0;
