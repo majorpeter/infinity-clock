811dcafc495dbb398f03bce2ac4b7b227e826595
Major Péter <majorpeter29@gmail.com>
2017-08-06 15:34:22 +0200

    added ledIndexOffset and reverse option to LedStripController, added some colors for debug

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,21 @@ int main() {
 	Hardware::RTC_Init();
 	Hardware::IRQ_Init();
 
-	LedStripController ledStrip(Hardware::LedStripDataOutPort, Hardware::LedStripDataOutPin);
+	LedStripController ledStrip(Hardware::LedStripDataOutPort, Hardware::LedStripDataOutPin, 29, true);
 	ledStrip.init();
 
-	Color red(255, 0, 0);
+	Color colors[] = {
+			Color(255, 0, 0),
+			Color(55, 0, 0),
+			Color(0, 200, 0),
+			Color(2, 0, 222),
+			Color(155, 0, 100),
+			Color(25, 24, 24),
+			Color(0, 0, 0),
+			Color(0, 220, 220),
+	};
 	while(1) {
-		ledStrip.writeLeds(&red, 1);
+		ledStrip.writeLeds(colors, sizeof(colors) / sizeof(colors[0]));
 	}
 
 	return 0;
