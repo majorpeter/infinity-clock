3ac5c4c5f2170e2183f714222889c73996f97a91
Major Péter <majorpeter29@gmail.com>
2017-08-14 23:19:05 +0200

    created HTML5 canvas application to debug JsonCanvas output

---
+++ test/app.js
@@ -0,0 +1,40 @@
+var canvas;
+var frame = 0;
+function fillCircle(x, y, r, color) {
+   var ctx = canvas.getContext('2d');
+   ctx.beginPath();
+   ctx.arc(x, y, r, 0, 2 * Math.PI);
+   ctx.fillStyle = color;
+   ctx.fill();
+}
+
+function drawClockFace(led_data) {
+   var xy = 300;
+   var radius = 280;
+   for (var i = 0; i < 60; i++) {
+      var angle = (-Math.PI / 2) + (i * Math.PI / 30);
+      fillCircle(xy + Math.cos(angle) * radius, xy + Math.sin(angle) * radius, 9, led_data[i]);
+   }
+}
+
+function onLoad() {
+   // Get the canvas element using the DOM
+   canvas = document.getElementById('mycanvas');
+
+   // Make sure we don't execute when canvas isn't supported
+   if (canvas.getContext){
+      var interval = setInterval(function() {
+         if (json_data[frame]) {
+            drawClockFace(json_data[frame])
+         } else {
+            alert('Done');
+            clearInterval(interval);
+            return;
+         }
+         frame = frame + 1;
+      }, 16);
+   }
+   else {
+      alert('You need Safari or Firefox 1.5+ to see this demo.');
+   }
+}
