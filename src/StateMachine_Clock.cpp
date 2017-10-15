2bc9cc3f88e4f749c6c92f609b24a9a8d0d2f5b1
Major Péter <majorpeter29@gmail.com>
2017-10-15 19:51:38 +0200

    added animation for hour, closes #5; refactor

--- src/StateMachine_Clock.cpp
+++ src/StateMachine_Clock.cpp
@@ -9,38 +9,56 @@
 #include "Canvas.h"
 
 static const uint16_t secondAnimationDurationMs = 300;
-static const uint16_t minuteAnimationDurationMs = 1400;
+static const uint16_t minuteAnimationDurationMs = 1200;
+static const uint16_t hourAnimationDurationMs = 2800;
+
+// there are 60 leds, and 12 hours
+static const uint8_t ledsPerHour = (60 / 12);
 
 StateMachine_Clock::StateMachine_Clock() {
 }
 
 void StateMachine_Clock::render(Canvas& canvas, const Time& now) {
 	uint8_t hour = (now.getSec() / 3600) % 12;
-	uint8_t min = (now.getSec() / 60) % 60;
-	uint8_t sec = now.getSec() % 60;
-	uint16_t ms = now.getMsec();
+	uint8_t minute = (now.getSec() / 60) % 60;
+	uint8_t second = now.getSec() % 60;
+	uint16_t millisec = now.getMsec();
 
 	canvas.fillColor(Color::black);
 
-	int16_t hour_m1_led_index = hour * 5 - 1;
-	canvas.add(hour_m1_led_index, Color::red);
-	canvas.add(hour * 5, Color::red);
-	canvas.add(hour * 5 + 1, Color::red);
+	uint16_t minuteAnimationMs = second * 1000 + millisec;
+	int16_t hourCenterLedIndex = hour * ledsPerHour;
+	if ((minute == 0) && (minuteAnimationMs < hourAnimationDurationMs)) {
+		// [0 -> 1.0] transition state
+		float animState = (float) minuteAnimationMs / hourAnimationDurationMs;
+		// [0 .. 5], hourCenterLedIndex will be added to it
+		float centerPositionOffset = (1.f - animState) * ledsPerHour;
+		int16_t centerPositionOffsetIndex = (int16_t) centerPositionOffset;
+		float animationDifference = centerPositionOffset - centerPositionOffsetIndex;
 
-	uint16_t minuteAnimationMs = sec * 1000 + ms;
-	if (minuteAnimationMs < minuteAnimationDurationMs) {
-		float animState = (float) minuteAnimationMs / minuteAnimationDurationMs;
-		canvas.add(min - 1, Color::green * (1.f - animState));
-		canvas.add(min, Color::green * animState);
+		canvas.add(hourCenterLedIndex - centerPositionOffsetIndex - 2, Color::red * animationDifference);
+		canvas.add(hourCenterLedIndex - centerPositionOffsetIndex - 1, Color::red);
+		canvas.add(hourCenterLedIndex - centerPositionOffsetIndex, Color::red);
+		canvas.add(hourCenterLedIndex - centerPositionOffsetIndex + 1, Color::red * (1.f - animationDifference));
 	} else {
-		canvas.add(min, Color::green);
+		canvas.add(hourCenterLedIndex - 1, Color::red);
+		canvas.add(hourCenterLedIndex, Color::red);
+		canvas.add(hourCenterLedIndex + 1, Color::red);
 	}
 
-	if (ms < secondAnimationDurationMs) {
-		float animState = ((float) ms / (secondAnimationDurationMs));
-		canvas.add(sec - 1, Color::blue * (1.f - animState));
-		canvas.add(sec, Color::blue * animState);
+	if (minuteAnimationMs < minuteAnimationDurationMs) {
+		float animState = (float) minuteAnimationMs / minuteAnimationDurationMs;
+		canvas.add(minute - 1, Color::green * (1.f - animState));
+		canvas.add(minute, Color::green * animState);
 	} else {
-		canvas.add(sec, Color::blue);
+		canvas.add(minute, Color::green);
+	}
+
+	if (millisec < secondAnimationDurationMs) {
+		float animState = ((float) millisec / (secondAnimationDurationMs));
+		canvas.add(second - 1, Color::blue * (1.f - animState));
+		canvas.add(second, Color::blue * animState);
+	} else {
+		canvas.add(second, Color::blue);
 	}
 }
