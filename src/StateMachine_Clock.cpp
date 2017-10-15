ff4717e9103b7f806f5cb36b07cdd5211220f2b3
Major Péter <majorpeter29@gmail.com>
2017-10-15 18:58:41 +0200

    added animation for seconds display

--- src/StateMachine_Clock.cpp
+++ src/StateMachine_Clock.cpp
@@ -8,21 +8,31 @@
 #include "StateMachine_Clock.h"
 #include "Canvas.h"
 
+static const uint16_t secondAnimationDurationMs = 300;
+
 StateMachine_Clock::StateMachine_Clock() {
 }
 
 void StateMachine_Clock::render(Canvas& canvas, const Time& now) {
-	uint8_t hour = now.getSec() / 3600;
+	uint8_t hour = (now.getSec() / 3600) % 12;
 	uint8_t min = (now.getSec() / 60) % 60;
 	uint8_t sec = now.getSec() % 60;
+	uint16_t ms = now.getMsec();
 
 	canvas.fillColor(Color::black);
 
 	int16_t hour_m1_led_index = hour * 5 - 1;
-	canvas.add((hour_m1_led_index < 0) ? (hour_m1_led_index + 60) : hour_m1_led_index, Color::red);
+	canvas.add(hour_m1_led_index, Color::red);
 	canvas.add(hour * 5, Color::red);
 	canvas.add(hour * 5 + 1, Color::red);
 
 	canvas.add(min, Color::green);
-	canvas.add(sec, Color::blue);
+
+	if (ms < secondAnimationDurationMs) {
+		float animState = ((float) ms / (secondAnimationDurationMs));
+		canvas.add(sec - 1, Color::blue * (1.f - animState));
+		canvas.add(sec, Color::blue * animState);
+	} else {
+		canvas.add(sec, Color::blue);
+	}
 }
